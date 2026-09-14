0f4fd7bc478b30576c132d5d0af9d6dab1400e8c
JordanGrant3D <jordangrant3d@gmail.com>
2026-09-14 16:12:31 +0200

    Added f2 rename for objects in Structure

--- editor/window/Structure.cpp
+++ editor/window/Structure.cpp
@@ -41,6 +41,8 @@
 #include <unordered_map>
 #include <unordered_set>
 
+#include "../../libs/imgui/imgui.h"
+
 using namespace doriax;
 
 std::vector<Entity> editor::Structure::getTopLevelSelectedEntities(Entity draggedEntity) {
@@ -1567,6 +1569,39 @@ void editor::Structure::showTreeNode(editor::TreeNode& node) {
         strncpy(nameBuffer, node.name.c_str(), sizeof(nameBuffer) - 1);
         nameBuffer[sizeof(nameBuffer) - 1] = '\0';
         ImGui::OpenPopup("ContextMenu");
+    }
+
+    if (nodeHovered && ImGui::IsKeyDown(ImGuiKey_F2)) {
+        strncpy(nameBuffer, node.name.c_str(), sizeof(nameBuffer) - 1);
+        nameBuffer[sizeof(nameBuffer) - 1] = '\0';
+        ImGui::OpenPopup("RenameMenu");
+    }
+
+    if (ImGui::BeginPopup("RenameMenu")) {
+        ImGui::Text("Name:");
+
+        // Focus the next item on the frame the popup opens
+        if (ImGui::IsWindowAppearing()) {
+            ImGui::SetKeyboardFocusHere();
+        }
+
+        ImGui::PushItemWidth(200);
+        // Added ImGuiInputTextFlags_AutoSelectAll flag
+        if (ImGui::InputText("##ChangeNameInput", nameBuffer, IM_ARRAYSIZE(nameBuffer),
+                             ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll)) {
+            ImGui::CloseCurrentPopup();
+                             }
+
+        if (ImGui::IsItemDeactivatedAfterEdit()) {
+            if (nameBuffer[0] != '\0' && strcmp(nameBuffer, node.name.c_str()) != 0) {
+                if (node.isScene) {
+                    CommandHandle::get(project->getSelectedSceneId())->addCommandNoMerge(new SceneNameCmd(project, node.id, nameBuffer));
+                } else {
+                    CommandHandle::get(project->getSelectedSceneId())->addCommandNoMerge(new EntityNameCmd(project, project->getSelectedSceneId(), node.id, nameBuffer));
+                }
+            }
+        }
+        ImGui::EndPopup();
     }
 
     if (ImGui::BeginPopup("ContextMenu")) {
